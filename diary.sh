e5974d2506764ae837b59cf0206a16b192503529
Atsushi Toyama <atsushi.tohyama.160.333@gmail.com>
2022-09-30 18:39:40 +0900

    first commit

---
+++ diary.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# 今日の日記を作成するスクリプト。
+# 通常は１日に一度実行する。
+
+function getDayOfWeek () {
+    dow=$(date "+%w")
+    week=("日" "月" "火" "水" "木" "金" "土")
+    echo "${week[$dow]}曜日"
+}
+
+DIARY_ROOT_PATH="/Users/tym/src/toasa.github.io/diary"
+Y=$(date "+%Y")
+M=$(date "+%m")
+D=$(date "+%d")
+
+TODAY_DIARY="${DIARY_ROOT_PATH}/${Y}/${M}/${D}.md"
+
+# 新年のディレクトリを作成
+if [ ! -d "${DIARY_ROOT_PATH}/${Y}" ]; then
+    mkdir ${DIARY_ROOT_PATH}/${Y}
+    echo "Create a new year directory: ${Y}"
+fi
+
+# 新月のディレクトリを作成
+if [ ! -d "${DIARY_ROOT_PATH}/${Y}/${M}" ]; then
+    mkdir ${DIARY_ROOT_PATH}/${Y}/${M}
+    echo "Create a new month directory: ${M}"
+fi
+
+# 作成する日記の中身
+content=$(cat << EOF
+### $(getDayOfWeek)
+
+<img src="" width="500">
+
+<img src="" width="700">
+EOF
+)
+
+# 月末は歩数報告のセクションを追加
+next_day=$(date -v+1d +%d)
+if [ $next_day = "01" ];then
+    content+=$(cat << "EOF"
+
+
+### 今月の歩数
+EOF
+)
+fi
+
+# 今日の日記を作成
+if [ ! -e ${TODAY_DIARY} ]; then
+    echo "$content" > ${TODAY_DIARY}
+    echo "Create new diary: ${TODAY_DIARY}"
+fi
+
+code ${DIARY_ROOT_PATH}
